9ea195e9b3747eca5547db2e8af3dcc1d361f786
AWITI0301 <johnoketchawiti@gmail.com>
2022-09-18 23:33:30 +0300

    add top to bottom button in the about page

--- src/About.js
+++ src/About.js
@@ -1,5 +1,12 @@
 import React from 'react'
 import './About.css'
+import aboutImg from "./img/prof7.jpg";
+function About() 
+  //  Up To Top Btn
+    window.addEventListener("scroll", function(){
+      const upToTop = document.querySelector("a.bottom__to__top");
+       upToTop.classList.toggle("active", window.scrollY > 0)
+    });
 
 function About() {
   return (
